411fc6eb7efd8101b59a297258339d7b2a8a8c6a
unknown <archygia12@gmail.com>
2022-08-08 20:00:13 +0900

    First Commit

---
+++ lupa.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lupa Kata Sandi | Sacode</title>
+	<!-- BOOTSTRAP -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+<!-- CONTAINER START -->
+<div class="container">
+	<div class="row mt-5">
+		<div class="col-6 shadow border m-auto rounded p-5">
+			<form>
+				<div class="text-center">
+					<img src="images/logo-sacode.png" class="mb-4" width="120px" alt="Logo SaCode">
+				</div>
+				<div class="text-center mt-3">
+					<h1 class="fw-bold">SaCode</h1>
+				</div>
+				<div class="text-center mt-4">
+					<p class="text-muted">#PapuaCodingSchool #PapuaTechCommunity</p>
+				</div>
+				<div>
+					<h2 class="fw-bolder">Atur Ulang Kata Sandi</h2>
+					<hr>
+				</div>
+				<div class="form-group mb-3">
+					<label class="form-label text-muted" for="email">Masukkan Ulang Alamat Email Anda</label>
+					<input type="email" name="" class="form-control form-control-lg py-3" placeholder="Masukkan Email" id="email" required="">
+				</div>
+				<div class="form-group mb-3 d-grid">
+					<button type="Submit" class="btn btn-primary fw-bold btn-lg py-3">Kirim</button>
+				</div>
+				<div class="mb-5 text-center">
+					<p><a href="index.php">Login</a></p>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+<!-- CONTAINER END -->
+
+<!-- FOOTER START -->
+<footer>
+	<p class="mt-5 text-mute text-center"> Copyright @ 2022 Developed By Archy Gia Destara</p>
+</footer>
+<!-- FOOTER END -->
+
+<!-- JS BOOTSTRAP -->
+<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
---
+++ registrasi.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registrasi | Sacode</title>
+	<!-- BOOTSTRAP -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+<!-- CONTAINER START -->
+<div class="container">
+	<div class="row mt-5">
+		<div class="col-6 shadow border m-auto rounded p-5">
+			<form>
+				<div class="text-center">
+					<img src="images/logo-sacode.png" class="mb-4" width="120px" alt="Logo SaCode">
+				</div>
+				<div class="text-center mt-3">
+					<h1 class="fw-bold">SaCode</h1>
+				</div>
+				<div class="text-center mt-4">
+					<p class="text-muted">#PapuaCodingSchool #PapuaTechCommunity</p>
+				</div>
+				<div>
+					<h2 class="fw-bolder">Registration</h2>
+					<hr>
+				</div>
+				<div class="form-group mb-3">
+					<label class="form-label text-muted" for="nama">Nama Lengkap</label>
+					<input type="nama" name="" class="form-control form-control-lg py-3" placeholder="Masukkan Nama" id="nama" required="">
+				</div>
+				<div class="form-group mb-3">
+					<label for="email" class="form-label text-muted" for="">Email</label>
+					<input id="email" type="email" name="" class="form-control form-control-lg py-3" placeholder="Masukkan Email" required="">
+				</div>
+				<div class="form-group mb-3">
+					<label for="password1" class="form-label text-muted" for="">Kata Sandi</label>
+					<input id="password1" type="password1" name="" class="form-control form-control-lg py-3" placeholder="Masukkan Password" required="">
+				</div>
+				<div class="form-group mb-3">
+					<label for="password2" class="form-label text-muted" for="">Ulangi Kata Sandi</label>
+					<input id="password2" type="password2" name="" class="form-control form-control-lg py-3" placeholder="Ulangi Kata Sandi" required="">
+				</div>
+				<div class="form-group mb-3 d-grid">
+					<button type="Submit" class="btn btn-primary fw-bold btn-lg py-3">Login</button>
+				</div>
+				<div class="mb-5 text-center">
+					<p>Sudah punya akun? <a href="login.php">Login</a></p>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+<!-- CONTAINER END -->
+
+<!-- FOOTER START -->
+<footer>
+	<p class="mt-5 text-mute text-center"> Copyright @ 2022 Developed By Archy Gia Destara</p>
+</footer>
+<!-- FOOTER END -->
+
+<!-- JS BOOTSTRAP -->
+<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
